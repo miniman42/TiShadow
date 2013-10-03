b6395e17a776ce2665e0d1a5f7f71add12acfa14
James Sugrue <james.sugrue@avego.com>
2013-10-03 18:12:27 +0100

    Addition of further logic to download bundles.

--- app/Resources/api/Management.js
+++ app/Resources/api/Management.js
@@ -1,58 +1,112 @@
 
+var TiShadow = require("/api/TiShadow"),
+    Compression = require('ti.compression'),
+    log = require('/api/Log'), 
+    utils = require('/api/Utils');
+
 
 //start the management process, waiting for production updates
 exports.start = function(options){
-	
-	var BUNDLE_TIMESTAMP = "currentBundleTimestamp", 
-		MIN_APP_REVISION = "minAppRevision";
+    
+    var BUNDLE_TIMESTAMP = "currentBundleTimestamp", 
+        MIN_APP_REVISION = "minAppRevision";
 
-	Ti.App.addEventListener("carma:feature.toggle", function(toggleData){ 
+    Ti.App.addEventListener("carma:feature.toggle", function(toggleData){ 
        
         console.log('CARMIFY: Received feature toggle ');
- 	    var currentBundleTimestamp, minAppRevision, 
- 	    	currentAppVersion = Ti.App.Properties.getString('carma.revision');	
+        var currentBundleTimestamp, minAppRevision, 
+            currentAppVersion = Ti.App.Properties.getString('carma.revision');  
 
- 	    var toggles = JSON.parse(toggleData.data).featureToggle;
-		for(var i = 0; i < toggles.length; i++){
+        var toggles = JSON.parse(toggleData.data).featureToggle;
+        for(var i = 0; i < toggles.length; i++){
 
-    	  	if(toggles[i].featureName === BUNDLE_TIMESTAMP){
-    	  		currentBundleTimestamp = toggles[i].value;
-	       	}
-		  	if(toggles[i].featureName === MIN_APP_REVISION){
-    	  		minAppRevision = toggles[i].value;
-	       	}
+            if(toggles[i].featureName === BUNDLE_TIMESTAMP){
+                currentBundleTimestamp = toggles[i].value;
+            }
+            if(toggles[i].featureName === MIN_APP_REVISION){
+                minAppRevision = toggles[i].value;
+            }
         }
         console.log('CARMIFY: minAppRevision: ' + minAppRevision + ' vs ' + currentAppVersion);
-   	 	console.log('CARMIFY: currentBundleTimestamp: ' + currentBundleTimestamp);
-       
+        console.log('CARMIFY: currentBundleTimestamp: ' + currentBundleTimestamp);
+       if(minAppRevision <= currentAppVersion){
+            getLatestBundle(currentBundleTimestamp);
+        }
     });
 
 
-	Ti.App.addEventListener("carma:life.cycle.launch", function(){ 
-		//TODO: Record the version of the app in the preferences store.
-		//TODO: include a parameter from carma-splinter
-		console.log('CARMIFY: App Launched');
-	});
-	
-	Ti.App.addEventListener("carma:life.cycle.resume", function(){ 
-		console.log('App Resumed');
-		//apply change if necessary. Start with prepareUpdatedVersion()
-	});
+    Ti.App.addEventListener("carma:life.cycle.launch", function(){ 
+        //TODO: Record the version of the app in the preferences store.
+        //TODO: include a parameter from carma-splinter
+        console.log('CARMIFY: App Launched');
+    });
+    
+    Ti.App.addEventListener("carma:life.cycle.resume", function(){ 
+        console.log('App Resumed');
+        //apply change if necessary. Start with prepareUpdatedVersion()
+    });
 
-	setInterval(function(){
-			console.log("Checking production for new stuff now.......");
-		
-	},10000);
+    setInterval(function(){
+            console.log("Checking production for new stuff now.......");
+        
+    },10000);
 
 
-	//TODO: detect the best time to update the app... 
-	//should we have an 'not now' event? 
-	//prepareUpdatedVersion();
+    //TODO: detect the best time to update the app... 
+    //should we have an 'not now' event? 
+    //prepareUpdatedVersion();
 
     //CHECK NATIVE VERSION CAN BE ACCESSED FROM TIAPP.XML 
 
     //EXTRACT BUNDLE TIMESTAMP 
 
+};
+
+
+
+function getLatestBundle(bundleTimestamp){
+    alert('Getting bundle for ' + bundleTimestamp);
+
+    var updateUrl="http://developer.avego.com/libs/splinter/carma-splinter.zip";
+    loadRemoteZip("carma-splinter",updateUrl);
+  
+}
+
+
+
+function loadRemoteZip(name, url) {
+  var xhr = Ti.Network.createHTTPClient();
+  xhr.setTimeout(10000);
+  xhr.onload=function(e) {
+    try {
+      log.info("Unpacking new production bundle: " + name);
+
+      var path_name = name.replace(/ /g,"_");
+      // SAVE ZIP
+      var zip_file = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, path_name + '.zip');
+      zip_file.write(this.responseData);
+      // Prepare path
+      var target = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, path_name);
+      if (!target.exists()) {
+        target.createDirectory();
+      }
+      // Extract
+      var dataDir=Ti.Filesystem.applicationDataDirectory + "/";
+      Compression.unzip(dataDir + path_name, dataDir + path_name + '.zip',true);
+
+  
+      console.log("Launching...");
+      // Launch
+      TiShadow.launchApp(path_name);
+    } catch (e) {
+      log.error(utils.extractExceptionData(e));
+    }
+  };
+  xhr.onerror = function(e){
+    Ti.UI.createAlertDialog({title:'XHR', message:'Error: ' + e.error}).show();
+  };
+  xhr.open('GET', url);
+  xhr.send();
 };
 
 
@@ -64,22 +118,22 @@ exports.start = function(options){
  **/
 function prepareUpdatedVersion(){
 
-	var backupDir = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory,            
+    var backupDir = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory,            
     'standby');
     var sourceDir  =Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory,            
     'carma-splinter');
 
-	if (backupDir.exists()) {
-	    backupDir.deleteDirectory(true);
-	}	
+    if (backupDir.exists()) {
+        backupDir.deleteDirectory(true);
+    }   
 
-	if(sourceDir.exists()){
-		//IOS Solution 
-		copyDir(Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory), sourceDir, 'backup');
-	}
-	else{
-		console.log('No Source directory');
-	}
+    if(sourceDir.exists()){
+        //IOS Solution 
+        copyDir(Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory), sourceDir, 'backup');
+    }
+    else{
+        console.log('No Source directory');
+    }
     //Now apply Greg's stuff. 
 
 }
