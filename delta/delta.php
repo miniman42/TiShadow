13b2e27d7a0b3a0fb094abab9e72fcc13f95c7a0
Greg Balmer <greg.balmer@gmail.com>
2013-10-08 12:04:56 +0100

    Adding delta server php and readme

---
+++ delta/delta.php
@@ -0,0 +1,57 @@
+<?php
+
+function checkBundleExists($bundle) {
+  return file_exists($bundle);
+}
+
+function checkBundleParam($bundle) {
+  return (strlen($bundle)===10)&&is_numeric($bundle);
+}
+
+define('BUNDLE_NAME','carma-splinter.zip');
+
+$os= $_GET["os"];
+$src= $_GET["src"];
+$tgt= $_GET["tgt"];
+
+if (!(($os==='ios')||($os==='android'))){
+	echo "<pre>Usage: http://".$_SERVER['HTTP_HOST']. $_SERVER['SCRIPT_NAME']."?os=(ios|android)&src=&ltsource bundle timestamp&gt&tgt=&lttarget bundle timestamp&gt</pre>";
+	return;
+}
+
+if (!(checkBundleParam($src)&&checkBundleParam($tgt))){
+	echo"<pre>Source or target bundle invalid!</pre>";
+	return;
+}
+
+$src_bundle="./$src/$os/".BUNDLE_NAME;
+$tgt_bundle="./$tgt/$os/".BUNDLE_NAME;
+$del_bundle_dir="./delta/$src"."_"."$tgt/$os/";
+
+if (file_exists($del_bundle_dir.BUNDLE_NAME)){
+	//Serve already created file via xsendfile module..
+	header("X-Sendfile: $del_bundle_dir".BUNDLE_NAME);
+	header("Content-Type: application/octet-stream");
+	header("Content-Disposition: attachment; filename=\"".BUNDLE_NAME."\"");
+	return;
+
+} elseif (checkBundleExists($src_bundle)&&checkBundleExists($tgt_bundle)){
+	if(!file_exists($del_bundle_dir)){
+		mkdir($del_bundle_dir,0777,true);
+	}
+	//create delta bundle	
+	$output = shell_exec("export TMP=/var/www/tishadow/tmp/;tishadow delta -s $src_bundle -t $tgt_bundle -d $del_bundle_dir".BUNDLE_NAME);
+	if (strstr($output,'DONE')){
+		//Serve newly created bundle file via xsendfile module..
+		header("X-Sendfile: $del_bundle_dir".BUNDLE_NAME);
+		header("Content-Type: application/octet-stream");
+		header("Content-Disposition: attachment; filename=\"".BUNDLE_NAME."\"");
+	} else {	
+		echo "<pre>$output</pre>";
+	}
+	return;
+} else {
+	echo"<pre>Source or target bundle does not exist!</pre>";
+}
+
+?>
